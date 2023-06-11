8f3d8468deb80fe7502921c0638cfa71488cb9d2
Antonios F. Takos <adonitakos119@gmail.com>
2023-06-11 13:02:58 -0400

    Reverting last commit + requirements.txt

--- Resume_Parser.py
+++ Resume_Parser.py
@@ -1,20 +1,19 @@
-import flask
 from flask import Flask, request, render_template, send_from_directory, redirect
-# import PyPDF2
-# from docx import Document
+import PyPDF2
+from docx import Document
 import io
 import os
 import time
 import boto3
 from dotenv import load_dotenv
 
-# load_dotenv()
+load_dotenv()
 
 # s3 = boto3.resource(
 #     service_name = 's3',
 #     aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID'),
 #     aws_secret_access_key = os.environ.get('SECRET_ACCESS_KEY'),
-#     #bucket_name = os.environ.get('BUCKET_NAME')
+#     bucket_name = os.environ.get('BUCKET_NAME')
 # )
 
 # for bucket in s3.buckets.all():
