62901a3b115aa40e5529e69a7108c421f2096c2c
atthewmay5 <45432268+atthewmay5@users.noreply.github.com>
2023-06-11 00:17:05 -0400

    Committing update to file

--- Resume_Parser.py
+++ Resume_Parser.py
@@ -10,16 +10,36 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
-secret_access_key = os.environ.get('SECRET_ACCESS_KEY')
-bucket_name = os.environ.get('BUCKET_NAME')
+s3 = boto3.resource(
+    service_name = 's3',
+    aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key = os.environ.get('SECRET_ACCESS_KEY'),
+    #bucket_name = os.environ.get('BUCKET_NAME')
+)
 
-print(bucket_name)
+for bucket in s3.buckets.all():
+    print(bucket.name)
 
 directory = os.path.join(os.getcwd(), 'resumes')
 
 app = Flask(__name__)
 
+#file upload testing
+#def upload_file1():
+#    file = "D:\Downloads\matttestresume1.txt"
+#    s3.Bucket(bucket.name).upload_file(Filename=file, Key=file)
+#    return 'File uploaded successfully'
+
+#print all objects found in the AWS server
+#for obj in s3.Bucket(bucket.name).objects.all():
+#    print(obj)
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    file = request.files['file']
+    s3.Bucket(bucket.name).upload_file(Filename=file, Key=file)
+    return 'File uploaded successfully'
