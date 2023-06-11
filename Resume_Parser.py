48a20d295c2563cda64ba324c082b60a0177d71f
adonitakos <antonios.takos19@stjohns.edu>
2023-06-11 10:11:37 -0400

    CSS changes + fixes

--- Resume_Parser.py
+++ Resume_Parser.py
@@ -1,26 +1,26 @@
 import flask
 from flask import Flask, request, render_template, send_from_directory, redirect
-import PyPDF2
-from docx import Document
+# import PyPDF2
+# from docx import Document
 import io
 import os
 import time
 import boto3
 from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
-s3 = boto3.resource(
-    service_name = 's3',
-    aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key = os.environ.get('SECRET_ACCESS_KEY'),
-    #bucket_name = os.environ.get('BUCKET_NAME')
-)
+# s3 = boto3.resource(
+#     service_name = 's3',
+#     aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID'),
+#     aws_secret_access_key = os.environ.get('SECRET_ACCESS_KEY'),
+#     #bucket_name = os.environ.get('BUCKET_NAME')
+# )
 
-for bucket in s3.buckets.all():
-    print(bucket.name)
+# for bucket in s3.buckets.all():
+#     print(bucket.name)
 
-directory = os.path.join(os.getcwd(), 'resumes')
+# directory = os.path.join(os.getcwd(), 'resumes')
 
 app = Flask(__name__)
 
@@ -38,8 +38,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    file = request.files['file']
-    s3.Bucket(bucket.name).upload_file(Filename=file, Key=file)
-    return 'File uploaded successfully'
+# @app.route('/upload', methods=['POST'])
+# def upload_file():
+#     file = request.files['file']
+#     s3.Bucket(bucket.name).upload_file(Filename=file, Key=file)
+#     return 'File uploaded successfully'
+
+# Running Flask web application
+app.run(host='0.0.0.0', port=5000)
